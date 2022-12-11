ac08c0598966bc1137e673bcfe147e1a037c6746
Andrei Plotoaga <and1.plotoaga@gmail.com>
2022-12-11 18:06:40 +0100

    code refactored day 3

--- 3/normal_part2_3.cc
+++ 3/normal_part2_3.cc
@@ -29,13 +29,24 @@ unordered_map<char, int> generate_priority_map2()
     return priority;
 }
 
+template <typename T, typename Y>
+set<char> find_common_char(T & line1, Y & line2)
+{
+    set<char> common_chars {};
+    set<char> line1_set{begin(line1), end(line1)};
+    set<char> line2_set{begin(line2), end(line2)};
+
+    set_intersection(begin(line1_set), end(line1_set),
+                     begin(line2_set), end(line2_set),
+                     inserter(common_chars, begin(common_chars)));
+
+    return common_chars;
+}
 
 int find_matching_letters(ifstream & file, 
                           unordered_map<char, int> const& priority)
 {
-    string elf1 {};
-    string elf2 {};
-    string elf3 {};
+    string elf1, elf2, elf3;
 
     int total_value {};
 
@@ -44,27 +55,14 @@ int find_matching_letters(ifstream & file,
         getline(file, elf2);
         getline(file, elf3);
 
-        set<char> elem_elf1{begin(elf1), end(elf1)};
-        set<char> elem_elf2{begin(elf2), end(elf2)};
-        set<char> elem_elf3{begin(elf3), end(elf3)};
-
-        set<char> elf1_elf2{};
-        set_intersection(begin(elem_elf1), end(elem_elf1),
-                         begin(elem_elf2), end(elem_elf2),
-                         inserter(elf1_elf2, begin(elf1_elf2)));
-
-        set<char> final_set{};
-        set_intersection(begin(elf1_elf2), end(elf1_elf2),
-                         begin(elem_elf3), end(elem_elf3),
-                         inserter(final_set, begin(final_set)));
+        set<char> elf1_elf2{find_common_char(elf1, elf2)};
+        set<char> final_set{find_common_char(elf1_elf2, elf3)};
         
         char first = *begin(final_set);
         total_value += priority.at(first);
-
     }
 
     return total_value;
-
 }
 
 
@@ -78,5 +76,4 @@ int main()
     int value {find_matching_letters(file, priority)};
 
     cout << "Total value: " << value << endl;
-
 }
